1c2be40561758d5b52c8cae69be97c270c923792
Brian Hunter <brian@brianhunter.com>
2025-05-20 14:50:01 -0400

    fix production config for multiple page entry points

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        work: resolve(__dirname, 'work.html')
+      }
+    }
+  }
 })
+
